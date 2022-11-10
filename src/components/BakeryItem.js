d52666b8776a14cca3be7857e8fea3e1d09088d2
rampantshark777 <114953991+rampantshark777@users.noreply.github.com>
2022-11-09 21:39:39 -0500

    Add files via upload

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,24 @@
 // TODO: create a component that displays a single bakery item
+//import "src/assets/bakery-data.json";
+
+const BakeryItem = ({ name, description, price, image, addToCart }) => {
+  // const [open, setOpen] = useState(false);
+  // const [value, setValue] = useState(null);
+  // const [items, setItems] = useState([
+  //   { label: "Apple", value: "apple" },
+  //   { label: "Banana", value: "banana" },
+  // ]);
+  return (
+    // <View style={{ flexDirection: "row" }}>
+    <div>
+      <p>{name}</p>
+      <p>{description}</p>
+      <p>{price}</p>
+      <img src={image} alt="logo" />
+    </div>
+
+    // <FontAwesome name="sliders" style={styles.iconStyle} />
+  );
+};
+
+export default BakeryItem;
